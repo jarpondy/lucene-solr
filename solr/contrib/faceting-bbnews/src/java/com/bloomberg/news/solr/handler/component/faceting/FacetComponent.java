21d06cc85711c553cff2c697d89dfe0e683970df
jmarks47 <jmarks47@bloomberg.net>
2017-02-07 11:20:59 +0000

    FacetComponent: exclude terms from file in zk
    Test Plan: -
    
    Reviewers: dceccarelli4, cpoersch, dcollins
    
    Reviewed By: dceccarelli4
    
    Differential Revision: https://all.phab.dev.bloomberg.com/D828976

--- solr/contrib/faceting-bbnews/src/java/com/bloomberg/news/solr/handler/component/faceting/FacetComponent.java
+++ solr/contrib/faceting-bbnews/src/java/com/bloomberg/news/solr/handler/component/faceting/FacetComponent.java
@@ -17,6 +17,112 @@
 
 package com.bloomberg.news.solr.handler.component.faceting;
 
-public class FacetComponent extends org.apache.solr.handler.component.FacetComponent {
-    // This is a placeholder so that solrconfig.xml can reference FacetComponent.
+import org.apache.lucene.util.BytesRef;
+import org.apache.solr.common.SolrException;
+import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.common.util.NamedList;
+import org.apache.solr.core.SolrCore;
+import org.apache.solr.core.SolrResourceLoader;
+import org.apache.solr.handler.component.ResponseBuilder;
+import org.apache.solr.request.SimpleFacets;
+import org.apache.solr.search.DocSet;
+import org.apache.solr.request.SolrQueryRequest;
+import org.apache.solr.util.Predicate;
+import org.apache.solr.util.plugin.SolrCoreAware;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.*;
+
+public class FacetComponent extends org.apache.solr.handler.component.FacetComponent implements SolrCoreAware {
+    private final Map<String, String> fieldToExclusionFilenameMap;
+    private final Map<String, Set<BytesRef>> fieldToExcludesMap;
+
+    final static String CONFIG_FIELD_EXCLUSIONS = "fieldExclusions";
+    final static String CONFIG_FIELD_EXCLUSIONS_FIELD = "field";
+    final static String CONFIG_FIELD_EXCLUSIONS_FILE = "file";
+
+    public static Logger log = LoggerFactory.getLogger(FacetComponent.class);
+
+    public FacetComponent() {
+        this.fieldToExcludesMap = new HashMap<>();
+        this.fieldToExclusionFilenameMap = new HashMap<>();
+    }
+
+    @Override
+    public void init(NamedList args) {
+        super.init(args);
+
+        if (args == null) {
+            return;
+        }
+
+        final List<NamedList> fieldExclusions = args.getAll(CONFIG_FIELD_EXCLUSIONS);
+        for (NamedList fieldExclusion : fieldExclusions) {
+            final String field = (String) fieldExclusion.get(CONFIG_FIELD_EXCLUSIONS_FIELD);
+            final String file = (String) fieldExclusion.get(CONFIG_FIELD_EXCLUSIONS_FILE);
+
+            if (field == null || file == null) {
+                throw new SolrException(SolrException.ErrorCode.SERVER_ERROR,"Missing field/file name, args=" + args);
+            }
+
+            fieldToExclusionFilenameMap.put(field, file);
+        }
+
+        if (fieldToExclusionFilenameMap.isEmpty()) {
+            log.info("No exclusion files");
+        } else {
+            log.info("Exclusions={}", Arrays.toString(fieldToExclusionFilenameMap.entrySet().toArray()));
+        }
+    }
+
+    @Override
+    public void inform(SolrCore core) {
+        final SolrResourceLoader loader = core.getResourceLoader();
+
+        for (Map.Entry<String, String> fieldExclusion : fieldToExclusionFilenameMap.entrySet()) {
+            final String field = fieldExclusion.getKey();
+            final String file = fieldExclusion.getValue();
+
+            try {
+                final List<String> excludedTerms = loader.getLines(file);
+                final HashSet<BytesRef> bytesRefs = new HashSet<>(excludedTerms.size());
+                for (String term : excludedTerms) {
+                    bytesRefs.add(new BytesRef(term));
+                }
+                fieldToExcludesMap.put(field, bytesRefs);
+            } catch (IOException e) {
+                throw new SolrException(SolrException.ErrorCode.SERVER_ERROR, "Unable to read file", e);
+            }
+        }
+    }
+
+    @Override
+    protected SimpleFacets newSimpleFacets(SolrQueryRequest req, DocSet docSet, SolrParams params, ResponseBuilder rb) {
+        return new SimpleFacets(req, docSet, params, rb) {
+            @Override
+            protected Predicate<BytesRef> newExcludeBytesRefFilter(String field, SolrParams params) {
+                final Predicate<BytesRef> parentFilter = super.newExcludeBytesRefFilter(field, params);
+
+                final Set<BytesRef> excludeTerms = fieldToExcludesMap.get(field);
+                if (excludeTerms == null) {
+                    return parentFilter;
+                }
+
+                Predicate<BytesRef> filter = new Predicate<BytesRef>() {
+                    @Override
+                    public boolean test(BytesRef bytesRef) {
+                        return !excludeTerms.contains(bytesRef.utf8ToString());
+                    }
+                };
+
+                if (parentFilter != null) {
+                    filter = filter.and(parentFilter);
+                }
+
+                return filter;
+            }
+        };
+    }
 }
